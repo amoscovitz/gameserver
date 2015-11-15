fcb0990ff7fbdd297988e95df5a31cbefaf58850
amoscovitz <moscovitz@hotmail.com>
2015-11-15 23:01:28 +0100

    Create event.cpp

---
+++ server/event.cpp
@@ -0,0 +1,5 @@
+#include "event.h"
+
+const EventType EventData_MoveActor::sk_EventType(0x1EBE0D01);
+const EventType EventData_EndActor::sk_EventType(0x8EDE964A);
+const EventType EventData_ScoreActor::sk_EventType(0xB28347C0);
